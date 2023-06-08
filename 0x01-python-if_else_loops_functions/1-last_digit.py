f2abf4e1e7083c8ad43ac8e0aedbb22725e82059
Youssef <youssefkaramedu@gmail.com>
2023-06-08 05:38:14 -0700

    Fix output format

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,12 +3,12 @@ import random
 
 number = random.randint(-10000, 10000)
 
-print(f"Last digit of {number} is")
+print(f"Last digit of {number} is",  end = ' ')
 
 while (number > 10):
     number = number % 10
 
-print(f"{number} and is")
+print(f"{number} and is", end = ' ')
 
 
 if number > 5:
