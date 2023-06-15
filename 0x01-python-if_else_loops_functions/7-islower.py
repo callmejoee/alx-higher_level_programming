6128092d444601a381d2d8a9395368f66e11964d
Youssef <youssefkaramedu@gmail.com>
2023-06-15 11:54:17 -0700

    Add file 7

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
-    if c >= 97 and c < 123:
-        return true
+    if c >= 'a' and c =< 'z':
+        return True
     else:
-        return false
+        return False
