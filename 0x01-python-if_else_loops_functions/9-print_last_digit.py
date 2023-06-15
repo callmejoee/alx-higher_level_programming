435f10ba276652835e2ec0616c4aa1b880d122e5
Youssef <youssefkaramedu@gmail.com>
2023-06-15 12:59:47 -0700

    Fix negative number

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    while number > 10 or number < -10:
+    number = abs(number)
+    while number > 10:
         number = number % 10
     print(number, end='')
     return number
