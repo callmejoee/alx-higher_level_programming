13956d8ef31250cc0ffd0a2315a0ad61a2e393d5
Youssef <youssefkaramedu@gmail.com>
2023-06-16 14:29:51 -0700

    Add main == name file

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -2,11 +2,12 @@
 from magic_calculation_102 import add, sub
 
 
-def magic_calculation(a, b):
-    if a < b:
-        c = add(a, b)
-        for i in range(4, 7):
-            c = add(c, i)
-        return c
-    else:
-        return sub(a, b)
+if __name__ == "__main__":
+    def magic_calculation(a, b):
+        if a < b:
+            c = add(a, b)
+            for i in range(4, 7):
+                c = add(c, i)
+            return c
+        else:
+            return sub(a, b)
