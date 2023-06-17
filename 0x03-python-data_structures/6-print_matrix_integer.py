4eaee008b3b2b8edb7fb7680ba9e1fb809e3066c
Youssef <youssefkaramedu@gmail.com>
2023-06-17 11:53:59 -0700

    Try it with join

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
-            print("{:d} ".format(matrix[i][j]), end='')
-        print()
+    for row in matrix:
+        row_str = " ".join("{:d}".format(elem) for elem in row)
+        print(row_str)
