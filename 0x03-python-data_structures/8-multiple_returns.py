dabc1dd7b7057bf3058dba8c7d7476f91442f7cd
Youssef <youssefkaramedu@gmail.com>
2023-06-18 10:07:59 -0700

    Fix edge case

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if sentence is None:
-        return 0, None
+    if sentence is None or sentence == "":
+        return 0, ""
     return len(sentence), sentence[0]
