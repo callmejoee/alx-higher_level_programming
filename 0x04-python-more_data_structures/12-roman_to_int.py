4773d3b1d05aaec9fb8f35750f6dad40f26093eb
Youssef <youssefkaramedu@gmail.com>
2023-06-24 09:26:36 -0700

    Fix edge cases where small number comes before

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,9 +2,15 @@
 def roman_to_int(roman_string):
     roman_dic = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     result = 0
+    prev = 0
     for i in roman_string:
         for key, value in roman_dic.items():
             if i in key:
-                result += value
+                current = value
+                if current > prev:
+                    result += current - (2 * prev)
+                else:
+                    result += value
+                prev = current
     return result
 
