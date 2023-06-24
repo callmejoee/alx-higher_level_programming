6151b8694015750cd943c4273984fef1707ab61a
Youssef <youssefkaramedu@gmail.com>
2023-06-24 09:32:14 -0700

    Fix coding style

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,17 @@
 def roman_to_int(roman_string):
     if roman_string is None or not isinstance(roman_string, str):
         return 0
-    roman_dic = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    roman_dic =
+    {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+    }
+
     result = 0
     prev = 0
     for i in roman_string:
@@ -15,4 +25,3 @@ def roman_to_int(roman_string):
                     result += value
                 prev = current
     return result
-
