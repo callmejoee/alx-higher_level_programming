a7175bb918ff325529c7bd5992b0e85036dc0f74
Youssef <youssefkaramedu@gmail.com>
2023-07-05 12:29:11 -0700

    Add file 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" This is the square module """
+
+
+class Square:
+    """
+    This is a square class
+
+    Attributes: __size (int): size of the square
+    """
+
+
+    def __init__(self, size):
+        """
+        function to initalize the string
+
+        Args: size (int): size
+        """
+        self.__size = size
