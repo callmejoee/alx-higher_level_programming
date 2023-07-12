3f1971e2324d8c15c23ec4d3b54ebcfdaedec357
Youssef <youssefkaramedu@gmail.com>
2023-07-12 11:03:56 -0700

    Add documentation

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
-
 """
 This is a module that provides a function to find the method of object
 """
 
 
 def lookup(obj):
-    """
-    returns all methods and arguments
-    """
-
-    return(dir(obj))
+    """ returns all methods and arguments """
+    return dir(obj)
