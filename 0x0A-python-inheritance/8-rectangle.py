1c91d76c4c364c7bf6fee95ef33a971db9f6e132
Youssef <youssefkaramedu@gmail.com>
2023-07-12 16:20:13 -0700

    Add file

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -10,6 +10,6 @@ class BaseGeometry:
     def integer_validator(self, name, value):
         """ validates that value is an integer greater than 0 and is int """
         if not isinstance(value, int):
-            raise TypeError(f"{name} must be an integer")
+            raise TypeError("<name> must be an integer")
         if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+            raise ValueError("<name> must be greater than 0")
