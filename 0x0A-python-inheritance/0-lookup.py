71ed309a7ace0fde46e29a4383a365db6dc2689b
Youssef <youssefkaramedu@gmail.com>
2023-07-12 10:58:18 -0700

    Fix pycode

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -3,6 +3,8 @@
 """
 This is a module that provides a function to find the method of object
 """
+
+
 def lookup(obj):
     """
     prints all methods and arguments
@@ -10,4 +12,5 @@ def lookup(obj):
     Args: obj (obj): object
     returns: list
     """
+
     return(dir(obj))
