8705f5abaa85990c66701f0fea7fd7e7a85503ce
Youssef <youssefkaramedu@gmail.com>
2023-07-12 16:59:00 -0700

    Add file 100

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" This is the module for a kind of class """
+
+class MyInt(int):
+    def __init__(self, num):
+        self.num = num
+
+    def __eq__(self, other):
+        return super().__ne__(other)
+
+    def __ne__(self, other):
+        return super().__eq__(other)
