8bd29d6f3b07db1e9221af73cedca14d9b86d67e
Youssef <youssefkaramedu@gmail.com>
2023-07-14 11:43:35 -0700

    Add proper test cases

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
+""" This is a module for the add function """
+
+
 def add_integer(a, b=98):
-    if not isinstance(a, int) or not isinstance(a, float):
+    """ This is the add function """
+    if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")
-    if not isinstance(b, int) or not isinstance(b, float):
+    if not isinstance(b, int) and not isinstance(b, float):
         raise TypeError("b must be an integer")
-    return a + b
+    return int(a) + int(b)
