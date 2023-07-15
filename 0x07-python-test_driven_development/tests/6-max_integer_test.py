2f57254a09264d0a16f819ec2acb5bb68d5b5808
Youssef <youssefkaramedu@gmail.com>
2023-07-15 12:38:09 -0700

    Add working test cases

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,20 +1,25 @@
+#!/usr/bin/python3
 import unittest
+max_integer = __import__('6-max_integer').max_integer
 
 class TestMaxInteger(unittest.TestCase):
     
     def test_integer(self):
-        self.assertEqual(print(max_integer([1, 2, 3, 4])), 4)
-        self.assertEqual(print(max_integer([1, 5, 3, 4])), 5)
-        self.assertEqual(print(max_integer([1, 2, 123, 1222, 1222, 3, 4])), 1222)
-        self.assertEqual(print(max_integer([-1, -2, 3, 4])), 4)
-        self.assertEqual(print(max_integer([-1, -2, -3, -4])), -1)
-        self.assertEqual(print(max_integer([-41, -122, 0, -3, -4])), 0)
-        self.assertEqual(print(max_integer([1])), 1)
-        self.assertEqual(print(max_integer([]))
-        self.assertEqual(print(max_integer()), None)
+        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+        self.assertEqual(max_integer([1, 5, 3, 4]), 5)
+        self.assertEqual(max_integer([1, 2, 123, 1222, 1222, 3, 4]), 1222)
+        self.assertEqual(max_integer([-1, -2, 3, 4]), 4)
+        self.assertEqual(max_integer([-1, -2, -3, -4]), -1)
+        self.assertEqual(max_integer([-41, -122, 0, -3, -4]), 0)
+        self.assertEqual(max_integer([1]), 1)
+        self.assertEqual(max_integer([]), None)
+        self.assertEqual(max_integer(), None)
         
     def test_string(self):
-        self.assertEqual(print(max_integer(["banana", "apple"])), banana)
-        self.assertEqual(print(max_integer(["banana", "apple", "cat"])), cat)
-        self.assertEqual(print(max_integer(["banana", "Apple"])), banana)
-        self.assertEqual(print(max_integer(["banana", "apple", "Cat"])), banana)
+        self.assertEqual(max_integer(["banana", "apple"]), "banana")
+        self.assertEqual(max_integer(["banana", "apple", "cat"]), "cat")
+        self.assertEqual(max_integer(["banana", "Apple"]), "banana")
+        self.assertEqual(max_integer(["banana", "apple", "Cat"]), "banana")
+
+if __name__ == "__main__":
+    unittest.main()
