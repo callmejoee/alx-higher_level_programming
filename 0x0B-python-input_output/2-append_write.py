0bd1ce22fc22e6d2e8590cf465d9051c6397d26e
Youssef Karam <92326279+callmejoee@users.noreply.github.com>
2023-07-21 23:15:31 +0200

    Add return statement

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -5,4 +5,4 @@
 def append_write(filename="", text=""):
     """This is a function to append file"""
     with open(filename, 'a', encoding='utf-8') as file:
-        file.write(text)
+        return file.write(text)
