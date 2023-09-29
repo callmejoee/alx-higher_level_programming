964630d5a7994f8536eb171bd941f565f7793a60
Youssef Karam <92326279+callmejoee@users.noreply.github.com>
2023-09-29 03:00:55 +0200

    Update 5-post_params.sh

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # this send the required stuff
-curl -sX POST -d 'email=test@gmail.com&subject=I will always be here for PLD' "$1"
+curl -s -X POST -d 'email=test@gmail.com&subject=I will always be here for PLD' "$1"
