0df67215001f98d4c8f0502b88976344c2776d91
Youssef Karam <92326279+callmejoee@users.noreply.github.com>
2023-09-30 01:26:41 +0200

    Update 1-hbtn_header.py

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 import sys
 import urllib.request
-""" This is a code to get header """
+""" This is a code to get header takes in a URL, sends a request to the URL and displays
+the value of the X-Request-Id variable found in the header of the response. """
 if __name__ == "__main__":
     n = (sys.argv[1])
     with urllib.request.urlopen(n) as response:
