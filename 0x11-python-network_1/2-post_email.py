f6d0a26a4f8ba622114249c7bec90932b4f396ff
Youssef <youssefkaramedu@gmail.com>
2023-09-29 17:15:50 -0700

    Add file 2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -11,7 +11,8 @@ url = sys.argv[1]
 email = sys.argv[2]
 data = urllib.parse.urlencode({'email': email}).encode('utf-8')
 
-with urllib.request.urlopen(url, data) as response:
+request = urllib.request.Request(url, data)
+
+with urllib.request.urlopen(request) as response:
     body = response.read().decode('utf-8')
-    print("Response body:")
     print(body)
