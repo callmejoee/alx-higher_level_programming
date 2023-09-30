d2d002f848958af90f3c602833e052251a84e98f
Youssef <youssefkaramedu@gmail.com>
2023-09-29 18:17:37 -0700

    Add file 4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,7 +5,6 @@ url = 'https://alx-intranet.hbtn.io/status'
 
 response = requests.get(url)
 body = response.content
-utf8_content = body.decode('utf-8')
 
 print("Body response:")
 print("\t- type:", type(body))
