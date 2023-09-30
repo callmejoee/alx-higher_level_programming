ed8c0138750856cba5d148373be067a528b13bba
Youssef <youssefkaramedu@gmail.com>
2023-09-30 09:55:58 -0700

    Add file 10

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,16 @@
+username = "callmejoee"
+token = "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"
+
+url = 'https://api.github.com/callmejoee'
+
+headers = {
+    'Authorization': f'Token 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'
+}
+
+response = requests.get(url, headers=headers)
+data = response.json()
+
+if 'id' in data:
+    print("Your GitHub ID is:", data['id'])
+else:
+    print("Failed to retrieve your GitHub ID")
