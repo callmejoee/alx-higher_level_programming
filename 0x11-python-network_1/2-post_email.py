0a6f17cb1ef5001f5451db6e07babe432f1edc70
Youssef <youssefkaramedu@gmail.com>
2023-09-29 17:25:52 -0700

    Fix pycode

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -8,13 +8,13 @@ import urllib.request
 import urllib.parse
 
 if __name__ == "__main__":
-    
+
     url = sys.argv[1]
     email = sys.argv[2]
     data = urllib.parse.urlencode({'email': email}).encode('utf-8')
-    
+
     request = urllib.request.Request(url, data)
-    
+
     with urllib.request.urlopen(request) as response:
         body = response.read().decode('utf-8')
         print(body)
