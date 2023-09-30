c887a0aaa7467cd963a34575969c098f153a7a2f
Youssef <youssefkaramedu@gmail.com>
2023-09-29 17:37:02 -0700

    Add file 3

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" a Python script that takes in a URL and an email, sends a POST request
+to the passed URL with the email as a parameter,
+and displays the body of the response (decoded in utf-8)"""
+
+import sys
+import urllib.request
+import urllib.parse
+
+if __name__ == "__main__":
+
+    url = sys.argv[1]
+try:
+    with urllib.request.urlopen(url) as response:
+        body = response.read().decode('utf-8')
+        print(body)
+except urllib.error.HTTPError as e:
+    print("Error code: " e.code)
