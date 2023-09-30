7773698e02025b8badd9cceb950f519cc4498a4c
Youssef Karam <92326279+callmejoee@users.noreply.github.com>
2023-09-30 02:23:59 +0200

    Update 2-post_email.py

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -7,12 +7,14 @@ import sys
 import urllib.request
 import urllib.parse
 
-url = sys.argv[1]
-email = sys.argv[2]
-data = urllib.parse.urlencode({'email': email}).encode('utf-8')
-
-request = urllib.request.Request(url, data)
-
-with urllib.request.urlopen(request) as response:
-    body = response.read().decode('utf-8')
-    print(body)
+if __name__ == "__main__":
+    
+    url = sys.argv[1]
+    email = sys.argv[2]
+    data = urllib.parse.urlencode({'email': email}).encode('utf-8')
+    
+    request = urllib.request.Request(url, data)
+    
+    with urllib.request.urlopen(request) as response:
+        body = response.read().decode('utf-8')
+        print(body)
