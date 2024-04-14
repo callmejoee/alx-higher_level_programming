0bdab8271ffdbd8971ee209e2c9cc2ed72f38d0e
Youssef <youssefkaramedu@gmail.com>
2024-04-14 11:20:58 -0700

    Add rectangle tests

---
+++ 0x0C-python-almost_a_circle/tests/test_rectangle.py
@@ -0,0 +1,13 @@
+import unittest
+
+from models.rectangle import Rectangle
+
+class TestRectangle(unittest.TestCase):
+
+    def test_attributes(self):
+        r = Rectangle(10, 20, 5, 10, 1)
+        self.assertEqual(r.width, 10)
+        self.assertEqual(r.height, 20)
+        self.assertEqual(r.x, 5)
+        self.assertEqual(r.y, 10)
+        self.assertEqual(r.id, 1)
