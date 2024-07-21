982058da3aa502301d61ea5b5377c89c59e7d93f
Youssef <youssefkaramedu@gmail.com>
2024-07-21 15:00:46 +0000

    Fix file 1

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -5,10 +5,10 @@ const args = process.argv.slice(2);
 const file = args[0];
 const str = args[1];
 
-fs.writeFile(file, str, 'utf8', (err, data) => {
+fs.writeFile(file, str, 'utf8', (err) => {
   if (err) {
-    console.error('Error reading file:', err);
+    console.error('Error writing to file:', err);
     return;
   }
-  console.log(data);
+  console.log('File written successfully');
 });
