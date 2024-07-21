01003a8dfd0f831d48e5c4c676ea6687c1833691
Youssef <youssefkaramedu@gmail.com>
2024-07-21 14:20:28 +0000

    Add file 0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
 const args = process.argv.slice(2);
-const file = args[0);
+const file = args[0];
 
 fs.readFile(file, 'utf8', (err, data) => {
   if (err) {
@@ -10,4 +10,3 @@ fs.readFile(file, 'utf8', (err, data) => {
   }
   console.log('File contents:', data);
 });
-
